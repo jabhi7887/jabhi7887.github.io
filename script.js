da54e0f66d5dca9125973955bedb2c9726b3d5bb
Abhishek Jadhav <96537114+jabhi7887@users.noreply.github.com>
2025-06-28 17:21:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function closeSidebar() {
   const Sidebar = document.querySelector('.sidebar')
   Sidebar.style.display = 'none'
 }
+
+
+const menuItem = document.querySelectorAll('.navbar .item');
+menuItem.forEach(item => {
+  item.addEventListener('click', () => {
+    menuItem.forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
+
+
+  function closeSidebar() {
+    document.querySelector('.sidebar').style.display = 'none';
+  }
+
+  // Automatically close sidebar on nav item click
+  document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', () => {
+      closeSidebar();
+    });
+  });
+
+  document.querySelectorAll(".sidebar a").forEach(link => {
+    link.addEventListener("click", () => {
+      document.querySelector(".sidebar").style.display = "none";
+    });
+  });
+
+  
+
+
